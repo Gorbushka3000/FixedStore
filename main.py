f1a39e56240097c7b060624ca4ab2b190ec905ca
gorbushka <glebdeshar@gmail.com>
2024-03-15 15:48:11 +0300

    use poetry version control

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('Hello World!')
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/")
+def list_items():
+    return [
+        'Item1',
+        'Item2',
+    ]
+
+
+@app.get('/items/{items_id}')
+def get_item_by_id(item_id):
+    return {
+        'item': {
+            id: item_id
+        }
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
